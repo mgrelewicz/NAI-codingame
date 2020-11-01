e72a8bd1f1fcb943b4a491680c2e96542022bb5f
mgrelewicz <37018381+mgrelewicz@users.noreply.github.com>
2020-11-01 21:29:05 +0100

    Create Power_of_Thor-Episode_1.py

---
+++ Power_of_Thor-Episode_1.py
@@ -0,0 +1,47 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+# ---
+# Hint: You can use the debug stream to print initialTX and initialTY, if Thor seems not follow your orders.
+
+# light_x: the X position of the light of power
+# light_y: the Y position of the light of power
+# initial_tx: Thor's starting X position
+# initial_ty: Thor's starting Y position
+light_x, light_y, initial_tx, initial_ty = [int(i) for i in input().split()]
+
+# game loop
+while True:
+    remaining_turns = int(input())  # The remaining amount of turns Thor can move. Do not remove this line.
+    if light_x > initial_tx AND light_y > initial_ty:
+        initial_tx += initial_tx
+        initial_ty += initial_ty
+        print("SE")
+        remaining_turns += remaining_turns
+        
+    elif light_x > initial_tx AND light_y < initial_ty:
+        initial_tx += initial_tx
+        initial_ty -= initial_ty
+        print("NE")
+        remaining_turns += remaining_turns
+
+    elif light_x < initial_tx AND light_y > initial_ty: 
+        initial_tx -= initial_tx
+        initial_ty += initial_ty
+        print("SW")
+        remaining_turns += remaining_turns
+
+    elif light_x = initial_tx AND light_y > initial_ty
+    else: 
+        print("NW")
+        remaining_turns += remaining_turns
+
+
+    # Write an action using print
+    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+
+    # A single line providing the move to be made: N NE E SE S SW W or NW
+    #print("SE")
