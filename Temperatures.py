4a943d23e8dd24f2c6a2121989b70691f3ee3f74
mgrelewicz <37018381+mgrelewicz@users.noreply.github.com>
2020-11-03 22:38:24 +0100

    Update Temperatures.py

--- Temperatures.py
+++ Temperatures.py
@@ -1,3 +1,4 @@
+# Marcin Grelewicz (s17692) NAI
 import sys
 import math
 
@@ -6,18 +7,33 @@ import math
 
 tmp = []
 value = 0
+result = 0
+result0 = 0
+result1 = 0
 
 n = int(input())  # the number of temperatures to analyse
 for i in input().split():
     # t: a temperature expressed as an integer ranging from -273 to 5526
     t = int(i)
-    tmp.append(t)
+    if t not in tmp:
+        tmp.append(t)
 
+if len(tmp) == 0:
+    result = 0
+elif len(tmp) == 1:
+    result = tmp[0]
+elif len(tmp) > 1:
+    result0 = sorted(tmp, key=lambda x:abs(x-value))[0]
+    result1 = sorted(tmp, key=lambda x:abs(x-value))[1]
 
-result = min(tmp, key=lambda x:abs(x-value))
+if abs(result0) == abs(result1):
+    result = abs(result0)
+elif abs(result0) != abs(result1):
+    result = result0
 
-
-
+#print(tmp)
+#print(result0)
+#print(result1)
 
 # Write an answer using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
