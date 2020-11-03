379efcc22df6692ed39362fc740f8df5cba29bda
mgrelewicz <37018381+mgrelewicz@users.noreply.github.com>
2020-11-03 20:23:26 +0100

    Update Power_of_Thor-Episode_1.py

--- Power_of_Thor-Episode_1.py
+++ Power_of_Thor-Episode_1.py
@@ -27,16 +27,16 @@ while 1:
         move_x = "W"
     elif light_x > current_tx:
         current_tx += 1
-        move_y = "E"
-   
-   if light_y < current_ty:
+        move_x = "E"
+
+    if light_y < current_ty:
         current_ty -= 1
         move_y = "N"
     elif light_y > current_ty:
         current_ty += 1
         move_y = "S"
     
-    print(move_y + move_x)
+    print(move_y+move_x)
 
     # Write an action using print
     # To debug: print("Debug messages...", file=sys.stderr, flush=True)
